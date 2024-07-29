e04ff3a009c2642114323b21ae15b56acbdc75d8
Jose Uriel Rodriguez Ramirez <uri_055@hotmail.com>
2024-07-29 15:29:26 -0600

    Adicion de bibliotecas frontend

--- Cross-site-scripting_(XSS)/demo/xss.php
+++ Cross-site-scripting_(XSS)/demo/xss.php
@@ -4,32 +4,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cross Site Script (XSS) Demo No Sanitizada</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
-<body>
-    <form  method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-        Name: <input type="text" name="name">
-        E-mail: <input type="text" name="email">
-    <button type="submit">Enviar</button>
-    </form>
+<body class="container">
+    <div class="row">
+        <form class="mt-3"  method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+            <div class="col-6">
+                <label for="name">Name:</label>
+                <input class="form-control" type="text" name="name">
+            </div>
+            <div class="col-6">
+                <label for="email">E-mail:</label>
+                <input class="form-control"  type="text" name="email">
+            </div>
+            
+            <button class=" mt-3 btn btn-primary" type="submit">Enviar</button>
+        </form>
+    </div>
 
-    <?php
-    $name = "";
-    $email = "";
+    <div class="mt-3">
+        <?php
+        $name = "";
+        $email = "";
 
-    //* Inject in the URL : /%22%3E%3Cscript%3Ealert('hacked')%3C/script%3E To execute an XSS vulnerability in xss.php
+        //* Inject in the URL : /%22%3E%3Cscript%3Ealert('hacked')%3C/script%3E To execute an XSS vulnerability in xss.php
 
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-    }
-    echo "<h2>Informacion por enviar</h2>";
-    echo "<p><strong>Nombre</strong></p>";
-    echo $name;
-    echo "<br>";
-    echo "<p><strong>Email</strong></p>";
-    echo $email;
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $name = $_POST["name"];
+            $email = $_POST["email"];
+        }
+        echo "<h2>Informacion por enviar</h2>";
+        echo "<p><strong>Nombre</strong></p>";
+        echo $name;
+        echo "<br>";
+        echo "<p><strong>Email</strong></p>";
+        echo $email;
 
-    ?>
+        ?>
+    </div>
+    
 </body>
 
 
--- Cross-site-scripting_(XSS)/demo/xss_sanitized.php
+++ Cross-site-scripting_(XSS)/demo/xss_sanitized.php
@@ -4,42 +4,55 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cross Site Script (XSS) Demo No Sanitizada</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
-<body>
-    <form  method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
-        Name: <input type="text" name="name">
-        E-mail: <input type="text" name="email">
-    <button type="submit">Enviar</button>
-    </form>
+<body class="container">
+    <div class="row">
+        <form class="mt-3"  method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+        <div class="col-6">
+                <label for="name">Name:</label>
+                <input class="form-control" type="text" name="name">
+            </div>
+            <div class="col-6">
+                <label for="email">E-mail:</label>
+                <input class="form-control"  type="text" name="email">
+            </div>
+            
+            <button class=" mt-3 btn btn-primary" type="submit">Enviar</button>
+        </form>
+    </div>
 
-    <?php
-    $name = "";
-    $email = "";
+    <div class="mt-3">
+        <?php
+        $name = "";
+        $email = "";
 
-    //* Inject in the URL : /%22%3E%3Cscript%3Ealert('hacked')%3C/script%3E To execute an XSS vulnerability in xss.php
-    //! Its important check all input from users and add all the methods to prevent XSS attacks.
-    
+        //* Inject in the URL : /%22%3E%3Cscript%3Ealert('hacked')%3C/script%3E To execute an XSS vulnerability in xss.php
+        //! Its important check all input from users and add all the methods to prevent XSS attacks.
+        
 
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $name = test_input($_POST["name"]);
-        $email = test_input( $_POST["email"]);
-    }
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $name = test_input($_POST["name"]);
+            $email = test_input( $_POST["email"]);
+        }
 
-    function test_input($user_data){
-        $user_data = trim($user_data);
-        $user_data = stripcslashes($user_data);
-        $user_data = htmlspecialchars($user_data);
-        return $user_data;
-    }
+        function test_input($user_data){
+            $user_data = trim($user_data);
+            $user_data = stripcslashes($user_data);
+            $user_data = htmlspecialchars($user_data);
+            return $user_data;
+        }
 
-    echo "<h2>Informacion por enviar</h2>";
-    echo "<p><strong>Nombre</strong></p>";
-    echo $name;
-    echo "<br>";
-    echo "<p><strong>Email</strong></p>";
-    echo $email;
+        echo "<h2>Informacion por enviar</h2>";
+        echo "<p><strong>Nombre</strong></p>";
+        echo $name;
+        echo "<br>";
+        echo "<p><strong>Email</strong></p>";
+        echo $email;
 
-    ?>
+        ?>
+    </div>
 </body>
 
 
